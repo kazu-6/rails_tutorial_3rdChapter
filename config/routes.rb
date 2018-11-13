8b45a66f218896c4d9e1dff19bb79eed2e40eae6
Kazuhiko Narita <pinkspider.6@gmail.com>
2018-11-13 23:49:15 +0000

    before starting test_3.3

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,10 @@
 Rails.application.routes.draw do
-  get 'static_pages/home'
+  get 'foo/bar'
 
-  get 'static_pages/help'
+  get 'foo/baz'
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  get 'static_pages/home'
+  get 'static_pages/help'
   root 'application#hello'
 end
