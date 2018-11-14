b78adc150ac331bcc70966d580ac45e42bfc1473
Kazuhiko Narita <pinkspider.6@gmail.com>
2018-11-14 04:58:18 +0000

    finished 3rd chapter

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,11 @@
 Rails.application.routes.draw do
-  get 'foo/bar'
 
-  get 'foo/baz'
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  root 'static_pages#home'
   get 'static_pages/home'
   get 'static_pages/help'
-  root 'application#hello'
+  get 'static_pages/about'
+  get 'static_pages/contact'
+
 end
