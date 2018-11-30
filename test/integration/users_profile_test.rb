859c902eba29bda050cf89fbdb9c5186d3240436
Kazuhiko Narita <pinkspider.6@gmail.com>
2018-11-30 03:39:57 +0000

    add user following

--- test/integration/users_profile_test.rb
+++ test/integration/users_profile_test.rb
@@ -20,4 +20,12 @@ class UsersProfileTest < ActionDispatch::IntegrationTest
     end
     assert_select 'div.pagination', content:1 
   end
+  
+  test "count relationships" do
+    log_in_as(@user)
+    get root_path
+    assert_match @user.active_relationships.count.to_s, response.body
+    assert_match @user.passive_relationships.count.to_s, response.body
+  end
+    
 end
